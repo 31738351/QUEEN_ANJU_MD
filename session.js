7c9120ed98aa47e4983633219fe80f1dcf2f1d9b
31738351 <165435707+31738351@users.noreply.github.com>
2024-11-22 09:02:28 -0500

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=SRl0VYbQ#fDm34izkxgGPzPJlOwsbHGBgoWrU96d3-_0tfYmsd4c",
 };
